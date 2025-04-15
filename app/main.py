0a0d4e9b544da8bfb49331349eaa56006b196b09
Jaisankar Selvam <89118783+jaisankar07@users.noreply.github.com>
2025-04-15 10:59:28 +0530

    checking-ui-DOCKER

--- app/main.py
+++ app/main.py
@@ -1,14 +1,12 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template
 from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return jsonify({
-        "timestamp": datetime.utcnow().isoformat() + "Z",
-        "ip": request.remote_addr
-    })
+def home():
+    current_time = datetime.now().strftime("%A, %d %B %Y %I:%M:%S %p")
+    return render_template("index.html", time=current_time)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
