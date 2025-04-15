7d9326f60e2b3b0b3b26235c65433956f337ade5
Jaisankar Selvam <89118783+jaisankar07@users.noreply.github.com>
2025-04-15 11:20:20 +0530

    Updated-program

--- app/main.py
+++ app/main.py
@@ -1,12 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route("/")
+def get_client_ip():
+    if request.headers.getlist("X-Forwarded-For"):
+        ip = request.headers.getlist("X-Forwarded-For")[0]
+    else:
+        ip = request.remote_addr
+    return ip
+
+@app.route('/')
 def home():
-    current_time = datetime.now().strftime("%A, %d %B %Y %I:%M:%S %p")
-    return render_template("index.html", time=current_time)
+    current_time = datetime.now().strftime("%H:%M:%S")
+    user_ip = get_client_ip()
+    return render_template('index.html', time=current_time, ip=user_ip)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
